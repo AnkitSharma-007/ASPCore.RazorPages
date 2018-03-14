8843abd59e669affa53e50cbf31fb4960b5aa2ff
AnkitSharma-007 <ankit.1si10cs010@gmail.com>
2018-03-14 22:00:03 +0530

    First commit

---
+++ EmployeeDemo/Pages/CreateEmployee.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using EmployeeDemo.Models;
+
+namespace EmployeeDemo.Pages
+{
+    public class CreateEmployeeModel : PageModel
+    {
+        EmployeeDataAccessLayer objemployee = new EmployeeDataAccessLayer();
+
+        [BindProperty]
+        public Employee employee { get; set; }
+
+        public ActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            objemployee.AddEmployee(employee);
+
+            return RedirectToPage("./EmployeeIndex");
+        }
+    }
+}
